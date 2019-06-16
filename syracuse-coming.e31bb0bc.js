3b678f39a0d0ac8f9ce2d746e5dc25024a6a709f
Gabriel Esu <mac@Gabriel-Esus-MacBook.local>
2019-06-16 14:36:18 +0100

    Updates

--- syracuse-coming.e31bb0bc.js
+++ syracuse-coming.e31bb0bc.js
@@ -12222,7 +12222,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64100" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50191" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
